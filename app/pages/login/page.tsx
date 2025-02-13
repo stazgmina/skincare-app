4bdae3c1f970e72d0c078c441f8dc4402d2dbabd
Morthmori <horidiano@gmail.com>
2025-02-13 13:39:12 +0100

    Enhance login page styling with gradient text and improved form layout

--- app/pages/login/page.tsx
+++ app/pages/login/page.tsx
@@ -11,7 +11,8 @@ const login = () => {
   return (
     <main className="flex flex-col md:flex-row">
       <section className="bg-[url('/banner.webp')]  bg-cover bg-center grid place-items-center p-8 order-2 md:order-1 h-[500px] md:h-screen w-screen md:w-1/2">
-        <h1 className='text-center text-4xl text-pink-800'>
+        
+        <h1 className='bg-gradient-to-r from-pink-600 to-pink-200 inline-block text-transparent bg-clip-text text-8xl text-center font-serif'>
           Join<br/> 
           the<br/>  
           Helldivers
@@ -21,7 +22,7 @@ const login = () => {
         <h1 className="text-2xl bg-gradient-to-r from-pink-500 to-pink-200 p-4 w-full max-w-[500px] text-white text-center rounded-full">
           Login
         </h1>
-        <form onSubmit={handleSubmit} className="grid place-items-center gap-2 w-full">
+        <form onSubmit={handleSubmit} className="grid place-items-center gap-4 w-full">
           <label htmlFor="username">Username:</label>
           <input type="text" id="username" name="username" required className="border-2 border-pink-200 rounded-full p-2 w-full max-w-[500px]"/>
           
