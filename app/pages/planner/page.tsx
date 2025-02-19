ecb74cdbd706c479ece32bb5b9f1053805f3352e
stazgmina <stazgmina@gmail.com>
2025-02-19 11:59:20 +0100

    Implement planner page layout with navigation and search functionality; remove unused transition utility file

--- app/pages/planner/page.tsx
+++ app/pages/planner/page.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { BsList } from "react-icons/bs"
 
 const Planner = () => {
   return (
-    <div>
-      <h1>Planner</h1>
-      <p>Plan your skincare routine here.</p>
-    </div>
+    <main>
+      <nav className='w-screen border grid place-items-center'>
+        <button>
+          <BsList size={40}/>
+        </button>
+      </nav>
+      <section>
+
+      </section>
+      <drawer className='border'>
+        <searchnav className="flex border w-screen gap-2">
+          <input type='text' placeholder='search' className='border'/>
+          <button className='border'>
+            Filter
+          </button>
+          <button className='border'>
+            Sort
+          </button>
+        </searchnav>
+        <productlist className=''>
+        </productlist>
+      </drawer>
+    </main>
   )
 }
 
