2f7f4767606401ca02b350f23e68703ed8645185
Morthmori <horidiano@gmail.com>
2025-02-25 10:20:41 +0100

    Enhance DayPlanner and SearchBar components for improved layout and styling; add react-icons dependency

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -4,9 +4,9 @@ import { BsSearch } from 'react-icons/bs'
 
 const SearchBar = () => {
   return (
-    <div className='flex items-center border border-pink-200 py-1 px-2 w-full md:w-auto bg-white text-pink-500 rounded-full' role='search'>
+    <div className=' gap-2 flex items-center border border-pink-200 py-1 px-2 w-full md:w-auto bg-white text-pink-500 rounded-full' role='search'>
       <BsSearch aria-label='search icon'/>
-      <input type='text' placeholder='Search' className='border-none outline-none ml-2 text-pink-500 rounded-full' aria-label='search input'/>
+      <input type='text' placeholder='Search' className='border-none outline-none  w-full text-pink-500 rounded-full' aria-label='search input'/>
     </div>
   )
 }
